7c8564ba1b01d43816ebd2aaca70dca7debd7d8c
Cinquin Andy <andy.cinquin@kleegroup.com>
2021-09-21 17:46:13 +0200

    corrections et petits changements

--- index.js
+++ index.js
@@ -30,22 +30,24 @@ function cookieParsing(){
     const cookies = await page.cookies()
     const cookieJson = JSON.stringify(cookies)
     fs.writeFileSync('cookies.json', cookieJson)
-    // ferme la page
+
+    // on ferme la page
     await page.close();
 
-    // ouvre une nouvelle page & set les cookies de sessions
+    // on ouvre une nouvelle page & set les cookies de sessions
     const deserializedCookies = cookieParsing();
-
-    // on passe dans une nouvelle page -> MEMORY CPU -> ID : 9
     const newPage = await browser.newPage();
     await newPage.goto(process.env.URL);
     await newPage.setCookie(...deserializedCookies)
 
+    // on va à la page de dashboard
     await newPage.goto(process.env.URL+process.env.LINK_PANNEL);
 
+    // on récupère tous nos blocs
     await newPage.waitForSelector('.panel-container');
     const labels = await newPage.$$(".panel-container");
 
+    // on récupère la liste de toutes les cards & on prend en particulier leurs titre [ contenus dans les "aria-label ]
     var objectList = [];
     var i=0;
     for( let label of labels ) {
@@ -60,7 +62,7 @@ function cookieParsing(){
         //    on click sur son nom pour dropdown le menu
         await newPage.evaluate(el => {el.click()}, main_element);
 
-        // On récupère le dropdown
+        // on récupère le dropdown
         await newPage.waitForSelector(`[aria-label="Panel header item Data"]`)
         let on_dropdown_element = await newPage.$(`[aria-label="Panel header item Data"]`)
         // on click sur le menu d'accès aux datas
@@ -79,5 +81,6 @@ function cookieParsing(){
         await newPage.evaluate(el => {el.click()}, on_left_element);
     }
 
-    // await browser.close();
+    // puis on ferme le navigateur
+    await browser.close();
 })();
