dc457f9eddd93f3521ff72e592e1f2dffd09cfd8
Cinquin Andy <andy.cinquin@kleegroup.com>
2021-09-22 11:20:23 +0200

    Complete Readme.md,and fix the script

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-klee_pypetter_grafana_auto_download_csv
+# Grafana Scraping project
+This projet is used to scrap a grafana instance and download all csv files from all charts in a specific pannel 
+
+## Prerequisite
+We need an functional instance of zabbix, connected with a grafana instance
+
+## Install
+First, you need to install node, and dependencies needed for this project  
+-> https://nodejs.org/en/download/  
+You’ll also need some packages that may or may not be available on your system. Just to be safe, try to install those:
+``
+sudo apt install -yq --no-install-recommends libasound2 libatk1.0-0 libc6
+libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4
+libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0
+libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcursor1
+libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1
+libxtst6 libnss3
+``
+---
+
+then, clone the project :   
+``sudo git clone git@github.com:CinquinAndy/klee_pypetter_grafana_auto_download_csv.git``   
+
+---
+
+Run this command in your project root directory:  
+``npm i puppeteer --save``
+
+---
+create a ``.env`` file  
+then complete all these informations :  
+```
+LOGIN="<yourGrafanaLogin>"
+PASSWORD="<yourPasswordLogin>"
+URL="http://<grafanaInstanceURL>"
+LINK_PANNEL="<URL_PannelThatYouWantToBeExported>"
+```
+example :  
+```
+LOGIN="admin"
+PASSWORD="adminpass"
+URL="http://192.168.172.131:3000"
+LINK_PANNEL="/d/41ZYbC7mz/zabbix-full-server-status"
+```
+
+## How to use
+Now you can just run the script with node
+````
+sudo node index.js
+````
+You will see all your downloaded files in `./csv` folder.
--- index.js
+++ index.js
@@ -1,45 +1,28 @@
 require('dotenv').config();
 const puppeteer = require('puppeteer');
-const fs = require('fs');
 const path = require("path");
-
-// todo regler le problème de browser headless qui n'arrive pas à récupèrer les éléments [ manque de js ? ]
-//https://stackoverflow.com/questions/49245080/how-to-download-file-with-puppeteer-using-headless-true
 
 (async () => {
     // on lance le launcher
     const browser = await puppeteer.launch(
         {
             // passer cette valeur à true plus tard
-            headless:true
+            headless: true
         }
     );
 
     // connexion
-    const page = await browser.newPage();
-    await page.goto(process.env.URL);
-
-    await page.type('input[name="user"]', process.env.LOGIN);
-    await page.type('input[name="password"]', process.env.PASSWORD);
-    await page.click('button[aria-label="Login button"]', {
-        button:"left",
-        clickCount:1
-    })
-
-    // save les cookies de sessions dans un fichier
-    const cookies = await page.cookies()
-    const cookieJson = JSON.stringify(cookies)
-    fs.writeFileSync('cookies.json', cookieJson)
-
-    // on ferme la page
-    await page.close();
-
-    // on ouvre une nouvelle page & set les cookies de sessions
-    const cookies_saved = fs.readFileSync('cookies.json', 'utf8')
-    const deserializedCookies = JSON.parse(cookies_saved);
     const newPage = await browser.newPage();
     await newPage.goto(process.env.URL);
-    await newPage.setCookie(...deserializedCookies)
+
+    await newPage.type('input[name="user"]', process.env.LOGIN);
+    await newPage.type('input[name="password"]', process.env.PASSWORD);
+    await newPage.click('button[aria-label="Login button"]', {
+        button: "left",
+        clickCount: 1
+    })
+
+    await newPage.goto(process.env.URL);
 
     const downloadPath = path.resolve('./csv/');
     await newPage._client.send('Page.setDownloadBehavior', {
@@ -49,7 +32,10 @@ const path = require("path");
     });
 
     // on va à la page de dashboard
-    await newPage.goto(process.env.URL+process.env.LINK_PANNEL);
+    await newPage.goto(process.env.URL + process.env.LINK_PANNEL, {
+        waitUntil: 'load'
+    });
+    await newPage.setViewport({width: 1280, height: 800});
 
     // on récupère tous nos blocs
     await newPage.waitForSelector('.panel-container');
@@ -57,8 +43,8 @@ const path = require("path");
 
     // on récupère la liste de toutes les cards & on prend en particulier leurs titre [ contenus dans les "aria-label ]
     var objectList = [];
-    var i=0;
-    for( let label of labels ) {
+    var i = 0;
+    for (let label of labels) {
         const attr = await newPage.evaluate(el => el.getAttribute("aria-label"), label);
         objectList[i] = attr;
         i++;
@@ -68,27 +54,35 @@ const path = require("path");
         await newPage.waitForSelector(`[aria-label="${objectList[i]}"] div.panel-title`)
         let main_element = await newPage.$(`[aria-label="${objectList[i]}"] div.panel-title`)
         //    on click sur son nom pour dropdown le menu
-        await newPage.evaluate(el => {el.click()}, main_element);
+        await newPage.evaluate(el => {
+            el.click()
+        }, main_element);
 
         // on récupère le dropdown
         await newPage.waitForSelector(`[aria-label="Panel header item Data"]`)
         let on_dropdown_element = await newPage.$(`[aria-label="Panel header item Data"]`)
         // on click sur le menu d'accès aux datas
-        await newPage.evaluate(el => {el.parentElement.click()}, on_dropdown_element);
+        await newPage.evaluate(el => {
+            el.parentElement.click()
+        }, on_dropdown_element);
 
         // on récupère le boutton de download
         await newPage.waitForSelector(`[aria-label="Panel inspector Data content"] div button span`)
         let on_button_element = await newPage.$(`[aria-label="Panel inspector Data content"] div button span`)
         // on click dessus
-        await newPage.evaluate(el => {el.click()}, on_button_element);
+        await newPage.evaluate(el => {
+            el.click()
+        }, on_button_element);
 
         // on récupère le drawer [ l'élément sur le coté du menu ]
         await newPage.waitForSelector(`div.drawer-mask`)
         let on_left_element = await newPage.$(`div.drawer-mask`)
         // on click dessus
-        await newPage.evaluate(el => {el.click()}, on_left_element);
+        await newPage.evaluate(el => {
+            el.click()
+        }, on_left_element);
     }
 
     // puis on ferme le navigateur
-    // await browser.close();
+    await browser.close();
 })();
